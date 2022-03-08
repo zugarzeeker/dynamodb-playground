0420eb8a94f5d25cc8e608774950edeedcbb2ef8
Supanut Apikulvanich <supanut.overload@gmail.com>
2022-03-08 15:32:16 +0700

    :sparkles: add index

---
+++ 04-add-index.js
@@ -0,0 +1,89 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb')
+const config = require('./config')
+const { p } = require('./utils')
+
+const TableName = process.env.TABLE_NAME || 'default_table'
+const userId = 'user_1'
+const groupId = 'group_1'
+
+const main = async () => {
+	const dynamodb = new DynamoDB(config)
+
+	console.log('====== create table ======')
+	await dynamodb.createTable({
+		TableName,
+		KeySchema: [
+			{ AttributeName: 'userId', KeyType: 'HASH' },
+			{ AttributeName: 'groupId', KeyType: 'RANGE' },
+		],
+		AttributeDefinitions: [
+			{ AttributeName: 'userId', AttributeType: 'S' },
+			{ AttributeName: 'groupId', AttributeType: 'S' },
+		],
+		GlobalSecondaryIndexes: [
+			{
+				IndexName: 'by_userId',
+				KeySchema: [
+					{ AttributeName: 'userId', KeyType: 'HASH' },
+				],
+				Projection: {
+					ProjectionType: 'ALL',
+				},
+			},
+		],
+		BillingMode: 'PAY_PER_REQUEST'
+	}).promise()
+
+	console.log('====== update table ======')
+	await dynamodb.updateTable({
+		TableName,
+		AttributeDefinitions: [
+			{ AttributeName: 'userId', AttributeType: 'S' },
+			{ AttributeName: 'groupId', AttributeType: 'S' },
+			{ AttributeName: 'userId-groupId', AttributeType: 'S' },
+		],
+		GlobalSecondaryIndexUpdates: [
+			{
+				Create: {
+					IndexName: 'by_user_group',
+					KeySchema: [
+						{ AttributeName: 'userId-groupId', KeyType: 'HASH' },
+					],
+					Projection: {
+						ProjectionType: 'ALL',
+					},
+				},
+			}
+		],
+	}).promise()
+	console.log('====== upsert item ======')
+
+	const item = {
+		TableName,
+		Item: {
+			userId: { S: userId },
+			groupId: { S: groupId },
+			'userId-groupId': { S: `${userId}-${groupId}` },
+			groupName: { S: 'AAA' }
+		}
+	}
+	p(item)
+	await dynamodb.putItem(item).promise();
+	
+	console.log('====== get item ======')
+	const record = await dynamodb.query({
+		TableName,
+		IndexName: 'by_user_group',
+		KeyConditionExpression: '#ugId = :value',
+		ExpressionAttributeNames: {
+			'#ugId': 'userId-groupId',
+		},
+		ExpressionAttributeValues: {
+			':value': { S: `${userId}-${groupId}` },
+		},
+		Limit: 1
+	}).promise();
+	p(record)
+}
+
+main().catch(console.error)
